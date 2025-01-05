afb821a1afbe3fb0adc78ee5c91e82a93f03a15a
PCPUN\USER <hakuojo@gmail.com>
2025-01-05 16:21:07 +0700

    เพิ่มการพิมพ์ตัวแปร y เข้าไป

--- Lab0701/C/LAB0701/LAB0701/LAB0701.cpp
+++ Lab0701/C/LAB0701/LAB0701/LAB0701.cpp
@@ -13,8 +13,18 @@ int main() {
     cout << "ค่าผ่าน ptr: " << *ptr << endl;   // เข้าถึงค่าผ่าน ptr
     cout << "ตำแหน่งใน ptr: " << ptr << endl;  // แสดง address ที่ ptr เก็บไว้
 
+
+    int y = 20;
+    ptr = &y; // เก็บ address ของ y
+
+    cout << "ค่าของ y: " << y << endl;           // แสดงค่าของ y
+    cout << "ตำแหน่งของ y: " << &y << endl;    // แสดง address ของ y
+    cout << "ค่าผ่าน ptr: " << *ptr << endl;   // เข้าถึงค่าผ่าน ptr
+    cout << "ตำแหน่งใน ptr: " << ptr << endl;  // แสดง address ที่ ptr เก็บไว้
+
     return 0;
 }
+
 
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
